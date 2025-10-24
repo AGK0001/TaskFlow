7b5b323c264881a4c1c956401e35b7aa10ae2bb1
AGK0001 <agk0001@alu.medac.es>
2025-10-24 11:23:23 +0200

    Refactoriza la vista de tareas a funciones (PE3)

--- app/views/header.php
+++ app/views/header.php
@@ -37,32 +37,8 @@
     <h1>Bienvenido a <?php echo SITE_NAME; ?></h1>
     <h2>Tareas Pendientes</h2>
     <ul>
-        <?php
-    foreach ($tasks as $task) {
-        // Clase base para cada tarea
-        $taskClasses = 'task-item';
-
-        // Si la tarea está completada, añade la clase 'completed'
-        if ($task['completed']) {
-            $taskClasses .= ' completed';
-        }
-
-        // Añade la clase de prioridad según el valor de 'priority'
-        switch ($task['priority']) {
-            case 'alta':
-                $taskClasses .= ' priority-alta';
-                break;
-            case 'media':
-                $taskClasses .= ' priority-media';
-                break;
-            case 'baja':
-                $taskClasses .= ' priority-baja';
-                break;
-        }
-
-        // Imprime la tarea como un elemento <li>
-        echo "<li class='$taskClasses'>{$task['title']}</li>";
-    }
-    ?>
+        <?php foreach ($tareas as $tarea) : ?>
+         <?php   echo renderizarTarea($tarea);?>    
+         <?php endforeach; ?>
     </ul>
 <main>
